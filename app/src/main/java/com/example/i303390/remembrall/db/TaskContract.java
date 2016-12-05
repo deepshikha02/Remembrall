59e1a9ef830f939e8ac7cd4342971f7e34fb8d4f
deepshikha02 <deepshikhamohanta@yahoo.com>
2016-12-05 17:41:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/i303390/remembrall/db/TaskContract.java
@@ -0,0 +1,19 @@
+package com.example.i303390.remembrall.db;
+
+/**
+ * Created by I303390 on 12/4/2016.
+ */
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.i303390.remembrall.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns{
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+
+    }
+}
